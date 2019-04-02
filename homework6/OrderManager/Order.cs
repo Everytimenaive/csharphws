5fb92b7f6c7f073b3e358c2f290d436b9c4ff327
noone <wanghr@waycloud.net>
2019-04-02 18:26:09 +0800

    homework6

---
+++ homework6/OrderManager/Order.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace OrderManager
+{
+    [Serializable]
+    public class Order: IComparable, IXmlSerializable
+    {
+        public int orderId { get; set; }
+        public string clientName { get; set; }
+        public OrderDetails details { get; set; }
+
+        public Order() { }
+
+        public Order(int orderId, string clientName)
+        {
+            this.orderId = orderId;
+            this.clientName = clientName;
+            details = new OrderDetails();
+        }
+
+        public override string ToString()
+        {
+            string str = orderId + " " + clientName + " " + details;
+            return str;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Order)
+            {
+                Order order = (Order)obj;
+                if (this.orderId == order.orderId || (this.clientName == order.clientName && this.clientName.Equals(order.details)))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return orderId;
+        }
+
+        public int CompareTo(Object obj)
+        {
+            if (!(obj is Order))
+            {
+                throw new ArgumentException("Wrong argument type!");
+            }
+            return this.orderId - ((Order) obj).orderId;
+        }
+
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            XmlSerializer intSerializer = new XmlSerializer(typeof(int));
+            XmlSerializer strSerializer = new XmlSerializer(typeof(string));
+            reader.ReadStartElement();
+            reader.ReadStartElement("orderId");
+            orderId = (int) intSerializer.Deserialize(reader);
+            reader.ReadEndElement();
+            reader.ReadStartElement("clientName");
+            clientName = (string) strSerializer.Deserialize(reader);
+            reader.ReadEndElement();
+            OrderDetails newDetails = new OrderDetails();
+            ((IXmlSerializable ) newDetails).ReadXml(reader);
+            details = newDetails;
+            reader.ReadEndElement();
+            reader.MoveToContent();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            XmlSerializer intSerializer = new XmlSerializer(typeof(int));
+            XmlSerializer strSerializer = new XmlSerializer(typeof(string));
+            writer.WriteStartElement("orderId");
+            intSerializer.Serialize(writer, orderId);
+            writer.WriteEndElement();
+            writer.WriteStartElement("clientName");
+            strSerializer.Serialize(writer, clientName);
+            writer.WriteEndElement();
+            writer.WriteStartElement("details");
+            ((IXmlSerializable) details).WriteXml(writer);
+            writer.WriteEndElement();
+        }
+    }
+}
---
+++ homework6/OrderManager/OrderDetails.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace OrderManager
+{
+    [Serializable]
+    public class OrderDetails: Dictionary<string, int>, IXmlSerializable
+    {
+        public int totalPrice
+        {
+            get => this.Values.Sum();
+        }
+
+        public OrderDetails() { }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (string key in this.Keys)
+            {
+                str += (key + ":" + this[key] + ";");
+            }
+            str = "(" + str + ")";
+            return str;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is OrderDetails)
+            {
+                OrderDetails orderDetails = (OrderDetails)obj;
+                if (this.ToString() == orderDetails.ToString())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        XmlSchema IXmlSerializable.GetSchema()
+        {
+            return null;
+        }
+
+        void IXmlSerializable.ReadXml(XmlReader reader)
+        {
+            XmlSerializer keySerializer = new XmlSerializer(typeof(string));
+            XmlSerializer valueSerializer = new XmlSerializer(typeof(int));
+            reader.ReadStartElement();
+            while(reader.NodeType != XmlNodeType.EndElement)
+            {
+                reader.ReadStartElement("OrderDetail");
+                reader.ReadStartElement("merchandise");
+                string key = (string) keySerializer.Deserialize(reader);
+                reader.ReadEndElement();
+                reader.ReadStartElement("amount");
+                int value = (int) valueSerializer.Deserialize(reader);
+                reader.ReadEndElement();
+                reader.ReadEndElement();
+                this.Add(key, value);
+                reader.MoveToContent();
+            }
+            reader.ReadEndElement();
+        }
+
+        void IXmlSerializable.WriteXml(XmlWriter writer)
+        {
+            XmlSerializer keySerializer = new XmlSerializer(typeof(string));
+            XmlSerializer valueSerializer = new XmlSerializer(typeof(int));
+            foreach(var pair in this)
+            {
+                writer.WriteStartElement("OrderDetail");
+                writer.WriteStartElement("merchandise");
+                keySerializer.Serialize(writer, pair.Key);
+                writer.WriteEndElement();
+                writer.WriteStartElement("amount");
+                valueSerializer.Serialize(writer, pair.Value);
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+            }
+        }
+    }
+}
